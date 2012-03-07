8a3708a241b63986e79a00a911abd0b7e69644d0
Ryan Boucher <ryan.boucher@distributedlife.com>
2012-03-07 17:03:43 +1100

    i think i've got it not

--- lib/cucumber_timing_presenter/configuration.rb
+++ lib/cucumber_timing_presenter/configuration.rb
@@ -2,7 +2,7 @@ module DistributedLife
   module CucumberTimingPresenter
     class Configuration
       def self.usage_template_path
-        File.expand_path("templates/all_usage_results.html", __FILE__)
+        File.expand_path("templates/all_usage_results.html", File.dirname(__FILE__))
       end
     end
   end
--- lib/cucumber_timing_presenter/version.rb
+++ lib/cucumber_timing_presenter/version.rb
@@ -1,5 +1,5 @@
 module DistributedLife
   module CucumberTimingPresenter
-    VERSION = "0.0.1.8"
+    VERSION = "0.0.1.9"
   end
 end
